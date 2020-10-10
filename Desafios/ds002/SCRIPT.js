bead95b95b4071c03a24b7a66292a32f6b0c7b80
João Pedro Souza da Cruz <joaopedropessoal.15@gmail.com>
2020-10-10 17:14:36 -0300

    Desafio 2

---
+++ Desafios/ds002/SCRIPT.js
@@ -0,0 +1,5 @@
+function desafio(){
+    var nome = prompt('Qual é o seu nome?')
+    var idade = prompt(`Olá ${nome}! Quantos anos você tem?`)
+    alert(`Acabei de conhecer ${nome}, que possui ${idade} anos de idade!`)
+}
